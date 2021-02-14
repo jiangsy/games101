201154bb0b6ff4494e0b5d90485c2845edc06843
jiangsy <shengyi.jiang@gmail.com>
2021-02-14 18:11:05 +0800

    finish pa4 bonus

--- pa4/code/main.cpp
+++ pa4/code/main.cpp
@@ -1,11 +1,11 @@
 #include <chrono>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <assert.h>
+
 
 std::vector<cv::Point2f> control_points;
 
-void mouse_handler(int event, int x, int y, int flags, void *userdata) 
+void mouse_handler(int event, int x, int y, int flags, void *userdata)
 {
     if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
     {
@@ -15,14 +15,14 @@ void mouse_handler(int event, int x, int y, int flags, void *userdata)
     }     
 }
 
-void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
+void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 {
     auto &p_0 = points[0];
     auto &p_1 = points[1];
     auto &p_2 = points[2];
     auto &p_3 = points[3];
 
-    for (double t = 0.0; t <= 1.0; t += 0.001) 
+    for (double t = 0.0; t <= 1.0; t += 0.001)
     {
         auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
                  3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
@@ -31,12 +31,12 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
     }
 }
 
-cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
+cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t)
 {
     std::vector<cv::Point2f> tmp_control_points;
     tmp_control_points.assign(control_points.begin(), control_points.end());
-    for (auto j=tmp_control_points.size(); j>0; j--) {
-        for (auto i=0; i < j-1; i++){
+    for (auto j=tmp_control_points.size(); j > 0; j--) {
+        for (auto i = 0; i < j-1; i++){
             tmp_control_points[i] = tmp_control_points[i]*t + tmp_control_points[i+1]*(1-t);
         }
     }
@@ -44,12 +44,28 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
 
 }
 
-void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
+void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
 {
-    for (double t = 0.0; t <= 1.0; t += 0.001)
-    {
+    auto radius = 0.5f;
+    auto cr = 0.7f;
+
+    for (double t = 0.0; t <= 1.0; t += 0.001) {
         auto point = recursive_bezier(control_points, t);
-        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+
+        int x1 = std::floor(point.x - radius);
+        int x2 = std::ceil(point.x + radius);
+        int y1 = std::floor(point.y - radius);
+        int y2 = std::ceil(point.y + radius);
+        for (int x = y1; x < y2; x++) {
+            for (int y = x1; y < x2; y++) {
+                auto dist = std::sqrt(std::pow(point.y - x - 0.5f, 2.0f) +
+                                      std::pow(point.x - y - 0.5f, 2.0f));
+                dist = dist / radius / 2.0f;
+                if (dist < 1.0f)
+                    dist = std::pow(dist, cr);
+                window.at<cv::Vec3b>(x, y)[1] = std::max(255 * (1.0f - dist), float(window.at<cv::Vec3b>(x, y)[1]));
+            }
+        }
     }
 }
 
@@ -69,16 +85,16 @@ int main()
             cv::circle(window, point, 3, {255, 255, 255}, 3);
         }
 
-        if (control_points.size() == 4) 
+        if (control_points.size() == 4)
         {
-            naive_bezier(control_points, window);
+//            naive_bezier(control_points, window);
             bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
-            key = cv::waitKey(0);
-
-            return 0;
+            cv::waitKey(0);
+            control_points.clear();
+            window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
         }
 
         cv::imshow("Bezier Curve", window);
